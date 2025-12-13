4d7d8e2bc0d831eb9331f50f03991d22ab131252
las-nish <19279535+las-nish@users.noreply.github.com>
2025-12-13 12:59:15 +0530

    feat: add new authentication-related paths

--- apps/frontend/constants/paths.ts
+++ apps/frontend/constants/paths.ts
@@ -1,5 +1,9 @@
 const PATHS = {
   SIGN_UP: '/sign-up',
+  SIGN_UP_CREATE: '/sign-up/create',
+  SIGN_IN: '/sign-in',
+  SIGN_IN_EMAIL: '/sign-in/email',
+  OVERVIEW: '/overview',
 };
 
 export { PATHS };
