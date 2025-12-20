f9ed7985af2b7148ddf9650133cc5ee336a77ef8
las-nish <19279535+las-nish@users.noreply.github.com>
2025-12-20 23:36:39 +0530

    feat: add logo to the error message

--- apps/frontend/components/ErrorMessage.ts
+++ apps/frontend/components/ErrorMessage.ts
@@ -1,23 +1,31 @@
+import { FlatIcon, FlatIconName } from './FlatIcon';
+
 type TOptions = {
   title: string;
   description: string;
 };
 
 const ErrorMessage = (opts: TOptions): HTMLElement => {
-  const containerDiv = document.createElement('div');
+  const overlayDiv = document.createElement('div');
+  const logoDiv = document.createElement('div');
+  const messageDiv = document.createElement('div');
   const titleDiv = document.createElement('div');
   const descriptionDiv = document.createElement('div');
 
-  containerDiv.classList.add('error-message');
+  overlayDiv.classList.add('error-message-overlay');
+  logoDiv.classList.add('error-message-logo');
+  messageDiv.classList.add('error-message');
   titleDiv.classList.add('error-message__title');
   descriptionDiv.classList.add('error-message__description');
 
-  containerDiv.append(titleDiv, descriptionDiv);
+  logoDiv.appendChild(FlatIcon(FlatIconName._26_WRITTTE_LOGO));
+  messageDiv.append(titleDiv, descriptionDiv);
+  overlayDiv.append(logoDiv, messageDiv);
 
   titleDiv.textContent = opts.title;
   descriptionDiv.textContent = opts.description;
 
-  return containerDiv;
+  return messageDiv;
 };
 
 export type { TOptions as TErrorMessageOptions };
