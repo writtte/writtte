0d705621575ea0061253a5bde1db2526cebb7871
las-nish <19279535+las-nish@users.noreply.github.com>
2025-12-21 00:35:43 +0530

    feat: implement close button component

---
+++ apps/frontend/components/CloseButton.ts
@@ -0,0 +1,34 @@
+import { setTestId } from '../utils/dom/testId';
+import { FlatIcon, FlatIconName } from './FlatIcon';
+
+type TOptions = {
+  id: string;
+  onClick: () => void;
+};
+
+type TReturnCloseButton = {
+  element: HTMLButtonElement;
+};
+
+const CloseButton = (opts: TOptions): TReturnCloseButton => {
+  const button = document.createElement('button');
+  button.classList.add('close-button');
+  button.appendChild(FlatIcon(FlatIconName._26_CROSS));
+
+  setTestId(button, opts.id);
+
+  button.addEventListener('click', (e: PointerEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    opts.onClick();
+  });
+
+  return {
+    element: button,
+  };
+};
+
+export type { TOptions as TCloseButtonOptions, TReturnCloseButton };
+
+export { CloseButton };
