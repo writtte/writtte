75ef36ff7fa03514c9d065e9a550ebbd443836e6
las-nish <19279535+las-nish@users.noreply.github.com>
2025-12-24 23:24:30 +0530

    refactor: move item setup flow into an api function

--- apps/frontend/components/EditorFixedMenu.ts
+++ apps/frontend/components/EditorFixedMenu.ts
@@ -6,14 +6,16 @@ import {
 
 type TOptions = {
   id: string;
-  items: (TEditorFixedMenuItemOptions & {
-    hasLeftDivider: boolean;
-    hasRightDivider: boolean;
-  })[];
 };
 
 type TReturnEditorFixedMenu = {
   element: HTMLMenuElement;
+  setItems: (
+    items: (TEditorFixedMenuItemOptions & {
+      hasLeftDivider: boolean;
+      hasRightDivider: boolean;
+    })[],
+  ) => void;
   returnsMap: {
     [key: string]: TReturnEditorFixedMenuItem;
   };
@@ -25,36 +27,44 @@ const EditorFixedMenu = (opts: TOptions): TReturnEditorFixedMenu => {
 
   const itemReturnsMap: { [key: string]: TReturnEditorFixedMenuItem } = {};
 
-  for (let i = 0; i < opts.items.length; i++) {
-    const itemElement = EditorFixedMenuItem(opts.items[i]);
+  const setItems = (
+    items: (TEditorFixedMenuItemOptions & {
+      hasLeftDivider: boolean;
+      hasRightDivider: boolean;
+    })[],
+  ): void => {
+    for (let i = 0; i < items.length; i++) {
+      const itemElement = EditorFixedMenuItem(items[i]);
 
-    if (
-      opts.items[i].hasLeftDivider !== undefined &&
-      opts.items[i].hasLeftDivider === true
-    ) {
-      const dividerDiv = document.createElement('div');
-      dividerDiv.classList.add('editor-fixed-menu__divider');
+      if (
+        items[i].hasLeftDivider !== undefined &&
+        items[i].hasLeftDivider === true
+      ) {
+        const dividerDiv = document.createElement('div');
+        dividerDiv.classList.add('editor-fixed-menu__divider');
 
-      menu.appendChild(dividerDiv);
+        menu.appendChild(dividerDiv);
+      }
+
+      menu.appendChild(itemElement.element);
+
+      if (
+        items[i].hasRightDivider !== undefined &&
+        items[i].hasRightDivider === true
+      ) {
+        const dividerDiv = document.createElement('div');
+        dividerDiv.classList.add('editor-fixed-menu__divider');
+
+        menu.appendChild(dividerDiv);
+      }
+
+      itemReturnsMap[items[i].id] = itemElement;
     }
-
-    menu.appendChild(itemElement.element);
-
-    if (
-      opts.items[i].hasRightDivider !== undefined &&
-      opts.items[i].hasRightDivider === true
-    ) {
-      const dividerDiv = document.createElement('div');
-      dividerDiv.classList.add('editor-fixed-menu__divider');
-
-      menu.appendChild(dividerDiv);
-    }
-
-    itemReturnsMap[opts.items[i].id] = itemElement;
-  }
+  };
 
   return {
     element: menu,
+    setItems,
     returnsMap: itemReturnsMap,
   };
 };
