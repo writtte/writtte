1c87d6700dc4cee3c1b6c8ecad03a2ad84a13d7b
las-nish <19279535+las-nish@users.noreply.github.com>
2026-01-04 20:03:04 +0530

    fix: fix paragraph wrapper for bullet/number list items when copying Medium.com export

--- packages/@writtte-editor/export/src/utils.ts
+++ packages/@writtte-editor/export/src/utils.ts
@@ -16,4 +16,36 @@ const tabsPrefixForListItem = (currentBlock: number): string => {
   return result;
 };
 
-export { normalizeNewlines, tabsPrefixForListItem };
+const extractParagraphContent = (text: string): string => {
+  const len = text.length;
+  if (len < 7) {
+    // "<p></p>" is minimum valid length
+
+    return text;
+  }
+
+  if (
+    text.charCodeAt(0) !== 60 ||
+    text.charCodeAt(1) !== 112 ||
+    text.charCodeAt(2) !== 62
+  ) {
+    // Check <p>
+
+    return text;
+  }
+
+  if (
+    text.charCodeAt(len - 4) !== 60 ||
+    text.charCodeAt(len - 3) !== 47 ||
+    text.charCodeAt(len - 2) !== 112 ||
+    text.charCodeAt(len - 1) !== 62
+  ) {
+    // Check </p>
+
+    return text;
+  }
+
+  return text.substring(3, len - 4);
+};
+
+export { normalizeNewlines, tabsPrefixForListItem, extractParagraphContent };
