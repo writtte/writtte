e04ef62e5050e26f89485f2209565dc1b3a7ba1f
𝔏𝔄𝔖𝔄𝔑 <19279535+las-nish@users.noreply.github.com>
2026-01-11 00:57:47 +0530

    Revert "[main] implement folder feature (Only DB+APIs)"

--- apps/backend/routes/private.go
+++ apps/backend/routes/private.go
@@ -14,8 +14,6 @@ func Private(mux *http.ServeMux) {
 	privateroutes.User(mux, &baseVersion)
 	privateroutes.Overview(mux, &baseVersion)
 	privateroutes.Item(mux, &baseVersion)
-	privateroutes.Folder(mux, &baseVersion)
-	privateroutes.Tree(mux, &baseVersion)
 	privateroutes.DocumentSharing(mux, &baseVersion)
 	privateroutes.Subscription(mux, &baseVersion)
 	privateroutes.Webhook(mux, &baseVersion)
--- databases/main/scripts/item/indexes/idx_tb_tree.pg.sql
+++
@@ -1,5 +0,0 @@
-\echo create index for (folder_code)
-CREATE INDEX idx_tree_folder ON schema_item.tb_tree (folder_code);
-
-\echo create index for (document_code)
-CREATE INDEX idx_tree_document ON schema_item.tb_tree (document_code);
