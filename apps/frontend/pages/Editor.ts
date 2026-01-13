73ec3ab37d9938cd0b2e3d827ee9fe687388c446
las-nish <19279535+las-nish@users.noreply.github.com>
2026-01-13 11:04:22 +0530

    Feat: configure editor options based on trial status

--- apps/frontend/pages/Editor.ts
+++ apps/frontend/pages/Editor.ts
@@ -45,7 +45,7 @@ const EditorPage = async (
 
   const editorElement = Editor({
     id: 'editor__oqvawzczdv',
-    options: setupEditorExtensionOptions(true),
+    options: setupEditorExtensionOptions(!isFreeTrialExpired),
   });
 
   if (editorElement) {
