45d99a87d7df96c4ddd089565563fac352461800
las-nish <19279535+las-nish@users.noreply.github.com>
2026-01-15 10:55:13 +0530

    refactor: remove unnecessary comments

--- databases/main/scripts/item/indexes/idx_tb_document.pg.sql
+++ databases/main/scripts/item/indexes/idx_tb_document.pg.sql
@@ -4,5 +4,5 @@ CREATE INDEX idx_document_account_lifecycle ON schema_item.tb_document (account_
 \echo create composite index for (account_code, lifecycle_state, workflow_state)
 CREATE INDEX idx_document_account_lifecycle_workflow ON schema_item.tb_document (account_code, lifecycle_state, workflow_state);
 
-\echo create index for (document_code) to efficiently retrieve e_tag
+\echo create index for (document_code)
 CREATE INDEX idx_document_code_etag ON schema_item.tb_document (document_code) INCLUDE (e_tag);
