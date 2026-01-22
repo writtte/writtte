469de9e370eaf769d3cdd3ae1e06dd1f9ed327f2
las-nish <19279535+las-nish@users.noreply.github.com>
2026-01-22 09:24:27 +0530

    feat: add manual credit amount to user creation process

--- databases/main/scripts/main/functions/v1_user_create.pg.sql
+++ databases/main/scripts/main/functions/v1_user_create.pg.sql
@@ -17,6 +17,8 @@ DECLARE
   v_check_user JSONB;
   v_account_code UUID;
   v_subscription_create_results JSONB;
+  v_p_manual_credit_amount NUMERIC(18, 6);
+  v_credit_add_results JSONB;
   v_exception TEXT;
 BEGIN
   v_p_email_address := lower(p_data ->> 'email_address');
@@ -25,6 +27,7 @@ BEGIN
   v_p_password_salt := (p_data ->> 'password_salt');
   v_p_limits := coalesce((p_data -> 'limits')::JSONB, '{}'::JSONB);
   v_p_usage := coalesce((p_data -> 'usage')::JSONB, '{}'::JSONB);
+  v_p_manual_credit_amount := (p_data ->> 'manual_credit_amount')::NUMERIC(18, 6);
   v_check_user := schema_main.v1_user_check (json_build_object('email_address', v_p_email_address)::JSONB, FALSE);
   IF v_check_user ->> k_code != 'USER_NOT_EXISTS' THEN
     RETURN v_check_user;
@@ -36,6 +39,12 @@ BEGIN
   v_subscription_create_results := schema_main.v1_subscription_create (json_build_object('account_code', v_account_code, 'status', 'NO_SUBSCRIPTION')::JSONB)::JSONB;
   IF (v_subscription_create_results ->> k_status)::BOOLEAN = FALSE THEN
     RAISE EXCEPTION 'failed to create subscription: %', v_subscription_create_results ->> k_message;
+  END IF;
+  IF v_p_manual_credit_amount IS NOT NULL THEN
+    v_credit_add_results := schema_main.v1_credit_add (json_build_object('account_code', v_account_code, 'manual_credit_amount', v_p_manual_credit_amount)::JSONB);
+    IF (v_credit_add_results ->> k_status)::BOOLEAN = FALSE THEN
+      RAISE EXCEPTION 'failed to add credits: %', v_credit_add_results ->> k_message;
+    END IF;
   END IF;
   RETURN json_build_object(k_status, TRUE, k_code, 'USER_CREATED', k_message, NULL, k_additional, NULL, k_data, json_build_object('account_code', v_account_code)::JSONB)::JSONB;
 EXCEPTION
