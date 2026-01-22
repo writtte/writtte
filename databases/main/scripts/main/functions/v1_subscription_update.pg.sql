70185d45496e7051f359eec62eb8e00d7133ed5a
las-nish <19279535+las-nish@users.noreply.github.com>
2026-01-22 09:24:39 +0530

    feat: add support for managing subscription credit amounts

--- databases/main/scripts/main/functions/v1_subscription_update.pg.sql
+++ databases/main/scripts/main/functions/v1_subscription_update.pg.sql
@@ -13,8 +13,10 @@ DECLARE
   v_p_service VARCHAR(32);
   v_p_service_data JSONB;
   v_p_status VARCHAR(16);
+  v_p_subscription_credit_amount NUMERIC(18, 6);
   v_updated_id BIGINT;
   v_updated_time TIMESTAMP WITH TIME ZONE;
+  v_credit_add_results JSONB;
   v_exception TEXT;
 BEGIN
   v_p_customer_id := (p_data ->> 'customer_id')::UUID;
@@ -22,6 +24,7 @@ BEGIN
   v_p_service := (p_data ->> 'service');
   v_p_service_data := (p_data -> 'service_data')::JSONB;
   v_p_status := upper(p_data ->> 'status');
+  v_p_subscription_credit_amount := (p_data ->> 'subscription_credit_amount')::NUMERIC(18, 6);
   UPDATE
     schema_main.tb_subscription
   SET
@@ -39,6 +42,12 @@ BEGIN
     v_updated_time;
   IF v_updated_id IS NULL THEN
     RETURN json_build_object(k_status, TRUE, k_code, 'SUBSCRIPTION_NOT_EXISTS', k_message, NULL, k_additional, NULL, k_data, NULL)::JSONB;
+  END IF;
+  IF v_p_subscription_credit_amount IS NOT NULL OR (p_data ->> 'reset_subscription_credit_amount')::BOOLEAN = TRUE OR (p_data ->> 'reset_manual_credit_amount')::BOOLEAN = TRUE THEN
+    v_credit_add_results := schema_main.v1_credit_add (json_build_object('account_code', p_account_code, 'subscription_credit_amount', v_p_subscription_credit_amount, 'reset_subscription_credit_amount', (p_data ->> 'reset_subscription_credit_amount')::BOOLEAN, 'reset_manual_credit_amount', (p_data ->> 'reset_manual_credit_amount')::BOOLEAN)::JSONB);
+    IF (v_credit_add_results ->> k_status)::BOOLEAN = FALSE THEN
+      RAISE EXCEPTION 'failed to add credits: %', v_credit_add_results ->> k_message;
+    END IF;
   END IF;
   RETURN json_build_object(k_status, TRUE, k_code, 'SUBSCRIPTION_UPDATED', k_message, NULL, k_additional, NULL, k_data, json_build_object('id_main', v_updated_id, 'account_code', p_account_code, 'status', v_p_status, 'updated_time', v_updated_time)::JSONB)::JSONB;
 EXCEPTION
