123d71dfaf18f1464aa652713cd66e7be9acfebe
las-nish <19279535+las-nish@users.noreply.github.com>
2026-01-26 15:39:03 +0530

    fix: drop analytics schema during database cleanup

--- databases/main/setup/clean.pg.sql
+++ databases/main/setup/clean.pg.sql
@@ -3,6 +3,9 @@ SET client_min_messages TO warning;
 \echo drop main schema
 DROP SCHEMA IF EXISTS schema_main CASCADE;
 
+\echo drop analytics schema
+DROP SCHEMA IF EXISTS schema_analytics CASCADE;
+
 \echo drop item schema
 DROP SCHEMA IF EXISTS schema_item CASCADE;
 
